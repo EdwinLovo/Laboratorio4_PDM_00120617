c2e375f3cec161163658c0f9fe9c4187ad5a124a
EdwinLovo <00120617@uca.edu.sv>
2019-04-05 11:08:19 -0600

    Segundo commit

--- app/src/main/java/com/example/pelicudex/MainActivity.kt
+++ app/src/main/java/com/example/pelicudex/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.pelicudex
 
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +20,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initRecyclerView()
+    }
+
+    fun addMovieToList(movie:Movie){
+        movieList.add(movie)
+        movieAdapter.changeList(movieList)
     }
 
     fun initRecyclerView(){
@@ -25,6 +36,36 @@ class MainActivity : AppCompatActivity() {
             layoutManager= viewManager
             //ADAPTER
             adapter = movieAdapter
+        }
+        add_movie_btn.setOnClickListener{
+            FetchMovie().execute("${movie_name_et.text}")
+        }
+    }
+
+    private inner class FetchMovie:AsyncTask<String, Void, String>(){
+        override fun doInBackground(vararg params: String): String {
+            if (params.isNullOrEmpty()) return  ""
+
+            val movieName = params[0]
+            val movieUrl = NetworkUtils().buildSearchUrl(movieName)
+            return  try {
+                NetworkUtils().getResponseFromHttpUrl(movieUrl)
+            }catch (e: IOException){
+                ""
+            }
+        }
+
+        override fun onPostExecute(movieInfo: String) {
+            super.onPostExecute(movieInfo)
+            if(!movieInfo.isEmpty()){
+                val movieJson = JSONObject(movieInfo)
+                if (movieJson.getString("Response")=="True"){
+                    val movie = Gson().fromJson<Movie>(movieInfo, Movie::class.java)
+                    addMovieToList(movie)
+                }else{
+                    Snackbar.make(main_ll, "No existe", Snackbar.LENGTH_SHORT).show()
+                }
+            }
         }
 
     }
